2ec5972cec22e82ebcb47e4af8d17d147cd25cb2
Jlopez <jonathanlopez@hotmail.ca>
2024-12-17 12:41:49 -0500

    adds logging to python-app

--- python-app/main.py
+++ python-app/main.py
@@ -1,15 +1,20 @@
 import grpc
 from flask import Flask, jsonify
 
+import logging
 import demo_pb2
 import demo_pb2_grpc
 
 app = Flask(__name__)
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
 CART_SERVICE_ADDRESS = "otel-shop-demo-cartservice.default.svc.cluster.local:8080"
 
 @app.route("/<user_id>")
 def get_cart(user_id):
+    logger.info(f"Getting cart for user {user_id}")
     with grpc.insecure_channel(CART_SERVICE_ADDRESS) as channel:
         stub = demo_pb2_grpc.CartServiceStub(channel)
         request = demo_pb2.GetCartRequest(user_id=user_id)
@@ -28,7 +33,16 @@ def get_cart(user_id):
 
 @app.route("/")
 def hello():
+    logger.info("root endpoint was reached")
     return "Python demo app", 200 
+
+@app.route('/error')
+def error():
+    try:
+        1 / 0
+    except ZeroDivisionError:
+        logger.error("An error occurred: Division by zero")
+    return jsonify(error="An error occurred"), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
