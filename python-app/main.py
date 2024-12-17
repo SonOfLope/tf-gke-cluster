286af9f9078f340d73f25f7aa05bc6099c31f346
Jlopez <jonathanlopez@hotmail.ca>
2024-12-17 14:48:50 -0500

    adds debug wit h reloader off

--- python-app/main.py
+++ python-app/main.py
@@ -45,4 +45,4 @@ def error():
     return jsonify(error="An error occurred"), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=False)
